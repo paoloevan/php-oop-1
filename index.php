d13adfd1d3dc11980d1600686ee3133b1f9fe6b0
paoloevan <paoloev1900@gmail.com>
2022-12-07 15:36:28 +0100

    add multiple genres

--- index.php
+++ index.php
@@ -7,26 +7,45 @@ class Movie
     public $director;
     public $duration;
     public $year;
+    public $genres;
 
-    function __construct($title, $director, $duration, $year)
+    function __construct($title, $director, $duration, $year, array $genres)
     {
         $this->title = $title;
         $this->director = $director;
         $this->duration = $duration;
         $this->year = $year;
+        $this->genres = $genres;
     }
 
-    public function get_title()
+    public function get_film_data()
     {
-        echo $this->title;
+        $data = [
+            'title' => $this->title,
+            'director' => $this->director,
+            'duration' => $this->duration,
+            'year' => $this->year,
+            'genres' => $this->genres
+        ];
+        foreach ($data as $item) {
+            if (!is_array($item)) {
+                echo $item;
+            } else {
+                foreach ($item as $genre) {
+                    echo $genre;
+                }
+            }
+        }
     }
 }
 
-$film_1 = new Movie('The blues brothers', 'john landis', 120, 1980);
+$genres_1 = ['musical', 'comedy', 'drama'];
+$film_1 = new Movie('The blues brothers', 'john landis', 120, 1980, $genres_1);
 var_dump($film_1);
 
-$film_2 = new Movie('Donnie darko', 'Richard Kelly', 100, 2001);
+$genres_2 = ['thriller', 'sci-fi', 'fantasy'];
+$film_2 = new Movie('Donnie darko', 'Richard Kelly', 100, 2001, $genres_2);
 var_dump($film_2);
 
-$film_1->get_title();
-$film_2->get_title();
+$film_1->get_film_data();
+$film_2->get_film_data();
