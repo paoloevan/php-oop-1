50d9c49bad04b26fdca58129ea06c14388c5d0d3
paoloevan <paoloev1900@gmail.com>
2022-12-07 17:05:28 +0100

    add db.php file

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php
+
+$bb_film = [
+    'title' => 'The blues brothers',
+    'directors' => 'john landis',
+    'duration' => 120,
+    'year' => 1980,
+    'genre' => [
+        'musical', 'comedy', 'drama'
+    ]
+];
+
+$donnie_film = [
+    'title' => 'Donnie darko',
+    'directors' => 'Richard Kelly',
+    'duration' => 100,
+    'year' => 2001,
+    'genre' => [
+        'thriller', 'sci-fi', 'fantasy'
+    ]
+];
--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 require __DIR__ . '/Models/Movie.php';
 require __DIR__ . '/Models/Genres.php';
+include __DIR__ . '/db.php';
 
-$genres_1 = new Genres(['musical', 'comedy', 'drama']);
-$film_1 = new Movie('The blues brothers', 'john landis', 120, 1980, $genres_1);
+$genres_1 = new Genres($bb_film['genre']);
+$film_1 = new Movie($bb_film['title'], $bb_film['director'], $bb_film['duration'], $bb_film['year'], $genres_1);
 var_dump($film_1);
 
-$genres_2 = new Genres(['thriller', 'sci-fi', 'fantasy']);
-$film_2 = new Movie('Donnie darko', 'Richard Kelly', 100, 2001, $genres_2);
+$genres_2 = new Genres($donnie_film['genre']);
+$film_2 = new Movie($donnie_film['title'], $donnie_film['director'], $donnie_film['duration'], $donnie_film['year'], $genres_2);
 var_dump($film_2);
 
 $film_1->get_film_data();
